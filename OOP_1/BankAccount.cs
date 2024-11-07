749694735b6c678e972c1cbde30603b8323fca7a
Denis Kartsev <thedofter@gmail.com>
2024-11-08 01:58:35 +0300

    all done

---
+++ OOP_1/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class BankAccount
+    {
+        private int _balance = 0;
+        private string? _ownerName;
+
+        public BankAccount(int startBalance)
+        {
+            Balance = startBalance;
+        }
+
+        public BankAccount(int startBalance, string owner)
+        {
+            Balance = startBalance;
+            OwnerName = owner;
+        }
+
+        public int Balance
+        {
+            get => _balance;
+            private set
+            {
+                if ((value >= 0) && (value <= int.MaxValue))
+                {
+                    _balance = value;
+                }
+                else if (value < 0)
+                {
+                    _balance = 0;
+                }
+            }
+        }
+
+        public void AddRubles(int rubles)
+        {
+            if (rubles <= 0)
+                return;
+
+            Balance += rubles;
+        }
+
+        public void WithdrawRubles(int rubles)
+        {
+            if (rubles <= 0)
+                return;
+
+            Balance -= rubles;
+        }
+
+        public string OwnerName
+        {
+            get => _ownerName ?? "NoName";
+            set { _ownerName = value; }
+        }
+    }
+}
---
+++ OOP_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, CPT!");
+
+            BankAccount bankAccount = new(199);
+            Console.WriteLine($"Аккаунт после инициализации без указания имени.");
+            Console.WriteLine($"Аккаунт \"{bankAccount.OwnerName}\". Баланс = {bankAccount.Balance}.");
+            bankAccount.AddRubles(200);
+            Console.WriteLine($"Аккаунт \"{bankAccount.OwnerName}\". Обновлённый баланс = {bankAccount.Balance}.");
+            bankAccount.OwnerName = "Тёмыч";
+            Console.WriteLine($"Тот же аккаунт с новым именем \"{bankAccount.OwnerName}\". Баланс = {bankAccount.Balance}.\n");
+
+            BankAccount bankAccount2 = new(0, "Denis");
+            Console.WriteLine($"Аккаунт после инициализации с указанием имени.");
+            Console.WriteLine($"Аккаунт \"{bankAccount2.OwnerName}\". Нищий баланс = {bankAccount2.Balance}.");
+            bankAccount2.AddRubles(5000);
+            Console.WriteLine($"Аккаунт \"{bankAccount2.OwnerName}\". УРА, СТИПУХА. Ваш баланс = {bankAccount2.Balance}.");
+            bankAccount2.WithdrawRubles(5000);
+            Console.WriteLine($"Аккаунт \"{bankAccount2.OwnerName}\". Вот и нет стипухи. Ваш баланс = {bankAccount2.Balance}.");
+        }
+    }
+}
