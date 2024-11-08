8f8d5441596802400b359e0c1761776d5f57e0b7
Denis Kartsev <thedofter@gmail.com>
2024-11-08 18:02:27 +0300

    solid task 3 :: all done

---
+++ SOLID_3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SOLID_3
+{
+    public abstract class Bird
+    {
+        public abstract string MoveToMe();
+    }
+
+    public class FlyingBird : Bird
+    {
+        public override string MoveToMe()
+        {
+            return "Я лечу к тебе!";
+        }
+    }
+
+    public class Penguin : Bird
+    {
+        public override string MoveToMe()
+        {
+            return "Я бегу к тебе!";
+        }
+    }
+
+    public class Program
+    {
+        public static void PrintComeToMe(Bird bird)
+        {
+            Console.WriteLine(bird.MoveToMe());
+        }
+
+        public static void Main()
+        {
+            Bird flyingBird = new FlyingBird();
+            Bird penguin = new Penguin();
+
+            PrintComeToMe(flyingBird);
+            PrintComeToMe(penguin);
+        }
+    }
+}
