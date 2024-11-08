8b4fe26bedc7fd73e58d59103993d8c22dd6bf0e
Denis Kartsev <thedofter@gmail.com>
2024-11-08 19:27:05 +0300

    generics task 5 :: all done

---
+++ GENERICS_5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GENERICS_5
+{
+    public interface IStorage<T>
+    {
+        void Add(T item);
+        T Get(int index);
+    }
+
+    public class ListStorage<T> : IStorage<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T Get(int index)
+        {
+            return _items[index];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storage = new ListStorage<int>();
+            storage.Add(42);
+            Console.WriteLine($"Первый элемент коллекции: {storage.Get(0)}");
+        }
+    }
+}
