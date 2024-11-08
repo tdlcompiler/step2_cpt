3ad5f6b7ffd42dd4b9451787314703dc1bcf2767
Denis Kartsev <thedofter@gmail.com>
2024-11-08 19:20:05 +0300

    generics task 4 :: all done

---
+++ GENERICS_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GENERICS_4
+{
+    public class Swapper
+    {
+        public static void Swap<T>(ref T first, ref T second)
+        {
+            (second, first) = (first, second);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine($"До свапа: a = {a}, b = {b}");
+            Swapper.Swap(ref a, ref b);
+            Console.WriteLine($"После свапа: a = {a}, b = {b}");
+        }
+    }
+}
