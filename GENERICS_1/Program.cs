d66036855eba5dca7f32c1d7d648e4b0ec8d8393
Denis Kartsev <thedofter@gmail.com>
2024-11-08 19:05:27 +0300

    generics task 1 :: al done

---
+++ GENERICS_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GENERICS_1
+{
+    public class Pair<T, U>
+    {
+        public T First { get; set; }
+        public U Second { get; set; }
+
+        public Pair(T first, U second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var pair = new Pair<int, string>(1, "один");
+            Console.WriteLine($"Пара {pair.First}:{pair.Second}");
+            pair.First = 12;
+            pair.Second = "двенадцать";
+            Console.WriteLine($"Изменённая пара {pair.First}:{pair.Second}");
+        }
+    }
+}
