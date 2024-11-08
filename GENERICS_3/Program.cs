61650beabd36c6e9c0b7db41912c06db26b50143
Denis Kartsev <thedofter@gmail.com>
2024-11-08 19:17:02 +0300

    generics task 3 :: all done

---
+++ GENERICS_3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GENERICS_3
+{
+    public class Repository<T> where T : class
+    {
+        private List<T> _items = new List<T>();
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T Get(int index)
+        {
+            return _items[index];
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var repo = new Repository<string>();
+            repo.Add("строчка");
+            Console.WriteLine(repo.Get(0));
+        }
+    }
+
+}
