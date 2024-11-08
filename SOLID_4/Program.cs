9e1d695c0acea51e9e582268b23ab5636e206dfe
Denis Kartsev <thedofter@gmail.com>
2024-11-08 18:11:58 +0300

    solid task 4 :: all done

---
+++ SOLID_4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SOLID_4
+{
+    public interface IPrinter
+    {
+        void Print();
+    }
+
+    public interface IScanner
+    {
+        void Scan();
+    }
+
+    public class MultiFunctionPrinter : IPrinter, IScanner
+    {
+        public void Print() => Console.WriteLine("Printing...");
+        public void Scan() => Console.WriteLine("Scanning...");
+    }
+
+    public class PrinterOnly : IPrinter
+    {
+        public void Print() => Console.WriteLine("Printing...");
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IPrinter printer = new PrinterOnly();
+            printer.Print();
+
+            IScanner scanner = new MultiFunctionPrinter();
+            scanner.Scan();
+            ((IPrinter)scanner).Print();
+        }
+    }
+}
